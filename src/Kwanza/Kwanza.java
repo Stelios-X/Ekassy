fff51778b3c65e76e09ccd3c8f59cfb884723027
Bobby Brandon Angote <ebobby@outlook.com>
2023-07-04 08:46:30 +0300

    First commit connection.

---
+++ src/Kwanza/Kwanza.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author user
+ *
+ */
+package Kwanza;
+
+public class Kwanza {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Mango mymango = new Mango();
+		mymango.Showme();
+
+	}
+
+}
---
+++ src/Kwanza/Mango.java
@@ -0,0 +1,10 @@
+package Kwanza;
+
+public class Mango 
+{
+	public void Showme() 
+	{
+		System.out.println("Khuvirre mwana Msilatsi!!");
+	}
+
+}
