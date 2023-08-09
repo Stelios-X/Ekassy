5c3d3a53fa32d19cb4a3afbfa40cffd840d2144d
Bobby Brandon Angote <ebobby@outlook.com>
2023-08-09 05:26:26 +0300

    Product of Array Except Self solution.

--- src/kazzy/Program.java
+++ src/kazzy/Program.java
@@ -5,10 +5,13 @@ public class Program {
 	public static void main(String[] args) 
 	{
 		 //myarr = {1,2,4,6,7};
-		 int[] prices = {7,1,6,4,2};
+		 int[] prices = {1,2,3,4};
 		 
-		 SecondSum newsum = new SecondSum();
-		 newsum.ObtainTwoSum(prices, 5);
+		 ProductOfArr mypr = new ProductOfArr();
+		 mypr.productExceptSelf(prices);
+		 
+		 //SecondSum newsum = new SecondSum();
+		// newsum.ObtainTwoSum(prices, 5);
 		// TODO Auto-generated method stub
 		/*SecondSum second_sum = new SecondSum();
 		second_sum.GetTwoSum(myarr, 10);*/
